8f049fa8d74b8052430a9ad81e96cf042dff5c23
dfried514 <dfried514@gmail.com>
2020-04-25 22:14:34 -0700

    more cleanup code

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import App from './App'
+import '@testing-library/jest-dom/extend-expect'
 
-render.test('renders learn react link', () => {
+test('renders learn react link', () => {
   const { getByText } = render(<App />)
   const linkElement = getByText(/learn react/i)
-  render.expect(linkElement).toBeInTheDocument()
+  expect(linkElement).toBeInTheDocument()
 })
